a302d5ff05a98419e91734f5a66f714d9f45e6f0
Adam <adambatukaev2003@mail.ru>
2023-06-05 11:59:23 +0300

    ts-js-to-ts

---
+++ books.ts
@@ -0,0 +1,15 @@
+interface IBooks {
+	title: string;
+	author: string;
+	year: number
+}
+const books: IBooks[] = [
+	{ title: '1984', author: 'George Orwell', year: 1949 },
+	{ title: 'Brave New World', author: 'Aldous Huxley', year: 1932 },
+];
+
+function getBooksByAuthor(author: string): IBooks[] {
+	return books.filter(book => book.author === author);
+}
+
+console.log(getBooksByAuthor('George Orwell'));
